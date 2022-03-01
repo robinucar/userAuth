d40589245d7312a6154b3ed687c68a23ec0e89ad
robin ucar <mehmet.ucar@imediapro.co.uk>
2022-03-01 11:25:32 +0000

    set ejs for view

--- app.js
+++ app.js
@@ -1,15 +1,22 @@
 // require express
-const express = require('express');
+const express = require("express");
+// require express layout for view
+const expressLayouts = require("express-ejs-layouts");
 
 const app = express();
 
+// EJS
+
+app.use(expressLayouts);
+app.set("view engine", "ejs");
+
 // call index routes
 
-app.use('/', require('./routes/index'))
+app.use("/", require("./routes/index"));
 
 // call users routes
 
-app.use('/users', require('./routes/users'))
+app.use("/users", require("./routes/users"));
 
 // setup port
 
